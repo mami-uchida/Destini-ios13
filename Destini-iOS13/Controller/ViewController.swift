29d6e7fc9954ddeea727c26d305c12874f6db564
内田麻美 <mamimami49192@gmail.com>
2023-02-03 19:09:39 +0900

    ＜コメント#1＞修正しました。

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var storyLabel: UILabel!
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
@@ -18,9 +18,10 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        storyLabel.sizeToFit()
         updateUI()
     }
-
+    
     @IBAction func choiceMade(_ sender: UIButton) {
         storyBrain.nextStory(userChoice: sender.currentTitle!)
         updateUI()
@@ -29,9 +30,9 @@ class ViewController: UIViewController {
         storyLabel.text = storyBrain.getStoryTitle()
         choice1Button.setTitle(storyBrain.getChoice1(), for: .normal)
         choice2Button.setTitle(storyBrain.getChoice2(), for: .normal)
-        }
     }
+}
 
-    
+
 
 
